9d48e673e159d688317408ec7521d3c91548c027
drazenstosic <87320413+drazenstosic@users.noreply.github.com>
2021-08-19 13:07:54 +0200

    Add last changes before working with server

---
+++ screens/Home.tsx
@@ -0,0 +1,15 @@
+import MainWrapper from "../components/MainWrapper";
+
+interface Props {}
+
+const Home = (props: Props) => {
+  return (
+    <MainWrapper>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </MainWrapper>
+  );
+};
+
+export default Home;
